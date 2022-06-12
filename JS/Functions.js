6bd5b0270dcf54eb4d030d506d2d8b0776050bf4
DiegoMorie <di.antivilo@duocuc.cl>
2022-06-12 13:23:44 -0400

    AdjustCss+JS_Start

--- JS/Functions.js
+++ JS/Functions.js
@@ -1,3 +1,46 @@
+function Show(){
+    const Style = document.getElementById('method_holder');
+    Style.style.opacity = 1;
+}
+
 function Ing_Rut(){
-    Base = document.getElementById('method_holder')
+    Show()
+    const Mod_login = document.getElementById('Log_type');
+    Mod_login.innerHTML = "Login con Rut" ;
+}
+
+
+function Check(Data,info){
+    switch (Data){
+        // Validación con RUT
+        case 1:
+            state = Val_RUT(info)
+            if (state == 0){
+                // ALERT something wrong
+            } else {
+                // Send to next page
+            }
+            break;
+
+
+        // Validación con Correo
+        case 2:
+            state = Val_Mail(info)
+            if (state == 0){
+                // ALERT something wrong
+            } else {
+                // Send to next page
+            }
+            break;
+
+        // Validación contraseña DuocUC
+        case 3:
+            state = Val_Pass(info)
+            if (state == 0){
+                // ALERT something wrong
+            } else {
+                // Send to next page
+            }
+            break;
+    }
 }
